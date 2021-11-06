5b806c78ff05a3d50120fe63fa50e1093f0eee8d
sasinduDias <sasiwijegunasinghe98@gmail.com>
2021-11-06 18:47:23 +0530

    email password checker

--- app/src/main/java/com/example/my/log1Activity.java
+++ app/src/main/java/com/example/my/log1Activity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class log1Activity extends AppCompatActivity {
     EditText et_email,et_password;
     RelativeLayout btn_login;
     TextView tv_sign_up;
+    String email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,27 @@ public class log1Activity extends AppCompatActivity {
 
         String sign_up_text="<font>Don't have an account?</font> <font color=#E26912><b> SIGNUP</b></font>";
         tv_sign_up.setText(Html.fromHtml(sign_up_text));
+        
+        
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email=et_email.getText().toString().trim();
+                password=et_password.getText().toString().trim();
+                Toast.makeText(log1Activity.this, "E mail is " + email + "" + "Password is" + password , Toast.LENGTH_SHORT).show();
+
+                if(email.isEmpty()){
+                    et_email.setError("Please fill the field!");
+                }else{
+                    if(password.isEmpty()){
+                        et_password.setError("please fill the field");
+                    }else{
+                        Toast.makeText(log1Activity.this, "ok" , Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
     }
 }
