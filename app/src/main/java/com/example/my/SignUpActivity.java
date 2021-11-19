1d7ee422ccffb8c2d55d7970a907dba2179e8560
sasinduDias <sasiwijegunasinghe98@gmail.com>
2021-11-19 22:06:58 +0530

    add

--- app/src/main/java/com/example/my/SignUpActivity.java
+++ app/src/main/java/com/example/my/SignUpActivity.java
@@ -1,8 +1,10 @@
 package com.example.my;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,10 +12,26 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUpActivity extends AppCompatActivity {
  TextView tv_login;
  RelativeLayout bt_signup;
  EditText et_username,et_email,et_contact_no,et_password;
+    private FirebaseAuth mAuth;
+    String UserId;
+    FirebaseFirestore db;
+
     String passwordPattern="[a-zA-Z0-9\\\\!\\\\@\\\\#\\\\$]{8,24}";
     String emailPattern="^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     String phonePattern="^[+]?[0-9]{10,13}$";
@@ -28,6 +46,11 @@ public class SignUpActivity extends AppCompatActivity {
         et_username=findViewById(R.id.edt_user);
         et_contact_no=findViewById(R.id.edt_contact_number);
         et_password=findViewById(R.id.edt_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        db = FirebaseFirestore.getInstance();
+
         String sign_up_text="<font>Already have an account?</font> <font color=#E26912><b> LogIn</b></font>";
         tv_login.setText(sign_up_text);
         
@@ -78,6 +101,40 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void saveUser(String user_name, String email, String phone, String password) {
        //firebase code
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+              //add data to the firebase store
+                UserId=mAuth.getCurrentUser().getUid();
+
+                DocumentReference documentReference=db.collection("USER").document(UserId);
+                Map<String, Object> user = new HashMap<>();
+                user.put("USER NAME", user_name);
+                user.put("EMAIL", email);
+                user.put("PHONE", phone);
+
+                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(SignUpActivity.this, "Login Success Data Saved" , Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignUpActivity.this,log1Activity.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUpActivity.this, "Login Success Data not Saved" , Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SignUpActivity.this, "Login not Success" , Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
