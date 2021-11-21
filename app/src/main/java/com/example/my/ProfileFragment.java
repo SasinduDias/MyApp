69504cc7c582ccd5bda4912259f8acfd8923857d
sasinduDias <sasiwijegunasinghe98@gmail.com>
2021-11-21 20:30:40 +0530

    add edit profile

--- app/src/main/java/com/example/my/ProfileFragment.java
+++ app/src/main/java/com/example/my/ProfileFragment.java
@@ -2,11 +2,24 @@ package com.example.my;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +27,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+    EditText et_name,et_email,et_phone;
+    Button bt_update,bt_logout;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
+    FirebaseFirestore db;
+    String Uid;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +78,55 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view= inflater.inflate(R.layout.fragment_profile, container, false);
+
+        et_name=view.findViewById(R.id.edt_name);
+        et_email=view.findViewById(R.id.edt_email);
+        et_phone=view.findViewById(R.id.edt_contact_number);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        Uid=mAuth.getCurrentUser().getUid();
+        user=mAuth.getCurrentUser();
+
+        DocumentReference documentReference=db.collection("USER").document(Uid);
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                         DocumentSnapshot documentSnapshot=task.getResult();
+                         if(documentSnapshot.exists())
+                         {
+                             String name,email,phone;
+                             name=(String) documentSnapshot.getData().get("USER NAME");
+                             email=(String) documentSnapshot.getData().get("EMAIL");
+                             phone=(String) documentSnapshot.getData().get("PHONE");
+                             setDetails(name,email,phone);
+
+                         }else{
+                            System.out.println("No such document");
+                         }
+                }else{
+                    Toast.makeText(getContext(), "Error Occurred!!" , Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getContext(), "Error Occurred!!" , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return view;
+    }
+
+    private void setDetails(String name, String email, String phone) {
+            et_name.setText(name);
+            et_email.setText(email);
+            et_phone.setText(phone);
+
     }
 }
